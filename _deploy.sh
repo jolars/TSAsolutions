5a50b9c25a4a250b795fdd37dcd02f81d1fb47dd
Johan Larsson <johanlarsson@outlook.com>
2017-03-20 22:13:11 +0100

    Test deploying with secure key.

--- _deploy.sh
+++ _deploy.sh
@@ -8,9 +8,11 @@ set -e
 git config --global user.email "johanlarsson@outlook.com"
 git config --global user.name "Johan Larsson"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
 cd book-output
 cp -r ../_book/* ./
-  git add --all *
-  git commit -m"Update the book" || true
+git add --all *
+git commit -m"Update the book"
 git push -q origin gh-pages
