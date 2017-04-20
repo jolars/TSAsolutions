57fccff570a027797814b1fae9cf722dcb542157
Johan Larsson <johanlarsson@outlook.com>
2017-04-20 20:07:54 +0200

    Try to use katex.

--- R/lattice_wrappers.R
+++ R/lattice_wrappers.R
@@ -1,5 +1,5 @@
-lat_acf <- function(x, ci = 0.95, ...) {
-  s <- TSA::acf(x, plot = FALSE, ...)
+lat_acf <- function(x, ci = 0.95, drop.lag.0 = FALSE, ...) {
+  s <- TSA::acf(x, drop.lag.0 = drop.lag.0, plot = FALSE, ...)
 
   nser <- ncol(s$lag)
 
@@ -12,6 +12,17 @@ lat_acf <- function(x, ci = 0.95, ...) {
                   ylim = ylim * 1.1,
                   panel = function(x, y, ...) {
                     panel.abline(h = c(-clim0, 0, clim0), lty = c(3, 1, 3))
+                    panel.xyplot(x, y, ...)
+                  })
+}
+
+tacf <- function(..., lag.max = 10) {
+  s <- ARMAacf(..., lag.max = lag.max)
+
+  lattice::xyplot(s ~ as.numeric(names(s)), type = "h",
+                  ylab = "ACF", xlab = "Lag",
+                  panel = function(x, y, ...) {
+                    panel.abline(h = 0)
                     panel.xyplot(x, y, ...)
                   })
 }
